e611c9b91caff289119f093ee2b3eb518cca0b26
MohamSayed <me.dev6789@gmail.com>
2020-03-18 14:09:44 +0200

    Firestore: delete

--- firestore_example/lib/main.dart
+++ firestore_example/lib/main.dart
@@ -4,39 +4,20 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Firestore Example'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -56,21 +37,21 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         body: Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
             child: StreamBuilder(
                 stream:
                     Firestore.instance.collection('testCollection').snapshots(),
                 builder: (context, snapshot) {
                   if (!snapshot.hasData)
                     return Text('Loading...');
-                  else
+                  else if (snapshot.data.documents.length > 0)
                     return ListView.builder(
                       padding: EdgeInsets.all(5),
                       itemCount: snapshot.data.documents.length,
                       itemBuilder: (context, index) => Dismissible(
                           // NOTE: key is very important for Dismissable widget
-                          onDismissed: (direction) {},
+                          onDismissed: (direction) {
+                            _deleteDocument(snapshot.data.documents[index]);
+                          },
                           background: Container(
                             color: Colors.redAccent,
                             alignment: Alignment(-0.8, 0.0),
@@ -85,6 +66,25 @@ class _MyHomePageState extends State<MyHomePage> {
                                   title: Text(snapshot.data.documents[index]
                                       ['name'])))),
                     );
+                  else
+                    return Text(
+                      "No documents in the db",
+                      style: TextStyle(color: Colors.redAccent),
+                    );
                 })));
+  }
+
+  // Deleting single document from firestore by transaction
+  _deleteDocument(document) {
+    Firestore.instance.runTransaction((Transaction transaction) async {
+      //
+      DocumentSnapshot nameSnapshot = await transaction.get(document.reference);
+      //
+      if (nameSnapshot.exists) {
+        print("Removing...");
+        // In transaction: you must call delete from transaction object
+        await transaction.delete(document.reference);
+      }
+    });
   }
 }
